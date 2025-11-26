05c912ff06dcc26fb64fb8f06ee7a11be8d13bca
Nihad <nihad.nemetli@gmail.com>
2025-11-26 15:04:02 +0400

    add: new constant created

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,33 +1,23 @@
 <?php
 
 declare(strict_types=1);
+ini_set("display_errors", 1);
+
+define("APP_ROOT", dirname(__DIR__));
 
 use GuzzleHttp\Psr7\ServerRequest;
 use HttpSoft\Emitter\SapiEmitter;
 use League\Route\Router;
-use App\Controllers\HomeController;
-use App\Controllers\ProductController;
-use Nyholm\Psr7\Factory\Psr17Factory;
-use Psr\Http\Message\ResponseFactoryInterface;
-use GuzzleHttp\Psr7\HttpFactory;
 use League\Route\Strategy\ApplicationStrategy;
-use Framework\Template\RendererInterface;
-use Framework\Template\Renderer;
-use Framework\Template\PlatesRenderer;
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\ORMSetup;
-use Doctrine\DBAL\DriverManager;
-use Doctrine\ORM\EntityManager;
 
-ini_set("display_errors", 1);
 
-require dirname(__DIR__) . "/vendor/autoload.php";
+require APP_ROOT . "/vendor/autoload.php";
 
 $request = ServerRequest::fromGlobals();
 
 $builder = new DI\ContainerBuilder;
 
-$builder->addDefinitions(dirname(__DIR__) . '/config/definitions.php');
+$builder->addDefinitions(APP_ROOT . '/config/definitions.php');
 
 $builder->useAttributes(true);
 
@@ -39,7 +29,7 @@ $strategy = new ApplicationStrategy;
 $strategy->setContainer($container);
 $router->setStrategy($strategy);
 
-$routes = require dirname(__DIR__) . '/config/routes.php';
+$routes = require APP_ROOT . '/config/routes.php';
 $routes($router);
 
 $response = $router->dispatch($request);
